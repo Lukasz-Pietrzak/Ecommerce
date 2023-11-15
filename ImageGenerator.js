78a8bad0d8090381a16dfa23e088676d1132e1dc
Lukasz-Pietrzak <bingo216pt@gmail.com>
2023-11-15 12:11:28 +0100

    Top-section

--- ImageGenerator.js
+++ ImageGenerator.js
@@ -1,16 +1,14 @@
 
-
-// import { numSpans } from './index.html';
-
 export default class ImageGenerator {
-    constructor(className, image, ImageType) {
+    constructor(imageNumber, className, image, ImageType) {
         this.className = className;
         this.image = image;
         this.ImageType = ImageType;
+        this.imageNumber = imageNumber;
     }
 
     generateImage() {
-        for (let i = 0; i <= numSpans; i++) {
+        for (let i = 0; i <= this.imageNumber; i++) {
             const element = document.getElementById("image" + i);
             if (element) {
                 element.innerHTML = `<img class='${this.className}' src='${this.image}${i}.${this.ImageType}' alt='Description of the image'>`;
